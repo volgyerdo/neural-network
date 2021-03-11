7f7c6416847d01a48c7df42b35e9e2f23ff385f1
pestiantal1 <pesti.antal1@gmail.com>
2021-03-11 23:59:22 +0100

    -Logic/ActivationLogic: integrated deactivate() module -Logic/ActivationFactory: createTanh, ReLu, LeakyRelu, Step and Linear added (TODO!)

--- src/volgyerdo.neural/classes/volgyerdo/neural/logic/ActivationFactory.java
+++ src/volgyerdo.neural/classes/volgyerdo/neural/logic/ActivationFactory.java
@@ -62,28 +62,88 @@ public class ActivationFactory {
         return activation;
     }
     
+    
+    //f(x)=((1+0.5*a)/(1+exp(-x))+b*x)+c
+//    a = swish
+//    b = slope
+//    c = shifty
     public static Activation createTanH() {
         Activation activation = new Activation();
+        //    a = swish: 2
+        //    b = slope: 0
+        //    c = shifty: -1
+        activation.shiftX = 0f;
+        activation.shiftY = -1f;
+        activation.stretchX = 1f; //mi is a deafult ertek? 1 vagy 0
+        activation.stretchY = 1f; // vagy ha nem kotelezo parameter nem is kell
+        activation.swish = 2f;
+        activation.slope = 0f;
+        
         return activation;
     }
     
     public static Activation createReLu() {
         Activation activation = new Activation();
+        //    a = swish: 100 (minél nagyobb annál jobb?)
+        //    b = slope: 0
+        //    c = shifty: 0
+        //      = shiftx: +11.6 (?)
+        activation.shiftX = 11.6f;
+        activation.shiftY = 0f;
+        activation.stretchX = 1f;
+        activation.stretchY = 1f;
+        activation.swish = 100f;
+        activation.slope = 0f;        
+        
         return activation;
     }
     
     public static Activation createLeakyReLu() {
-        Activation activation = new Activation();
+        Activation activation = new Activation();        
+        //    a = swish: 100 (minél nagyobb annál jobb?)
+        //    b = slope: 0 // "leakyness" ( 0.1 <= b <= 0.2 )
+        //    c = shifty: 0
+        //      = shiftx: +11.6 (?)
+        activation.shiftX = +11.6f;
+        activation.shiftY = 0f;
+        activation.stretchX = 1f;
+        activation.stretchY = 1f;
+        activation.swish = 1f;
+        activation.slope = 0f;
+        
         return activation;
     }
     
+    //f(x)=((((1+0.5*a)/(1+exp(-x))+b*x)+c) * d)
     public static Activation createStep() {
         Activation activation = new Activation();
+        //    a = swish: 100
+        //    b = slope: 0
+        //    c = shifty: 0
+        //    d = (jobb oldali slope maximum? skalár) = 0.019
+        // TODO: x sikon röviditeni 
+        activation.shiftX = 0f;
+        activation.shiftY = 0f;
+        activation.stretchX = 1f;
+        activation.stretchY = 1f;
+        activation.swish = 100f;
+        activation.slope = 0f;
+        
         return activation;
     }
     
     public static Activation createLinear() {
         Activation activation = new Activation();
+        //    a = swish: -2
+        //    b = slope: 1
+        //    c = shifty: 0
+        activation.shiftX = 0f;
+        activation.shiftY = 0f;
+        activation.stretchX = 1f;
+        activation.stretchY = 1f;
+        activation.swish = -2f;
+        activation.slope = 1f;
+        
         return activation;
     }
 
