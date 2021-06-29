31e7878f0a633131c14a879fab63dd95a5e20440
pestiantal <pesti.antal1@gmail.com>
2021-06-29 23:32:09 +0200

    LineRecognitionTest changes

--- src/volgyerdo.test/classes/volgyerdo/test/LineRecognitionTest.java
+++ src/volgyerdo.test/classes/volgyerdo/test/LineRecognitionTest.java
@@ -22,6 +22,7 @@ import java.util.Random;
 import java.util.Arrays;
 import volgyerdo.neural.logic.NetworkFactory;
 import volgyerdo.neural.structure.Network;
+import java.util.Collection;
 import volgyerdo.math.tensor.Tensor;
 import volgyerdo.neural.logic.ActivationFactory;
 import volgyerdo.neural.logic.LayerFactory;
@@ -39,18 +40,23 @@ import volgyerdo.neural.structure.Sample;
  */
 public class LineRecognitionTest {
 
+    private static final Random RANDOM_INT = new Random();
     private static int LINE_LENGHT = 3; //min0 max5
-    private static int LINE_COUNT = 2; //max4 min0
-    private static int TRAINING_SAMPLE_COUNT = 10;
+    private static int LINE_COUNT = 4; //min 0 max4
+    private static int TRAINING_SAMPLE_COUNT = 100;
     private static int ROWCOUNT = 5;
     private static final DecimalFormat FORMAT = new DecimalFormat("0.000");
 
     public static void main(String[] args) {
 
+        //network
         Network network = NetworkFactory.createNetwork(Tensor.TYPE.FLOAT);
         System.out.println("Start...");
 
-        NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 25));
+        NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 5,5));
+        NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
+        NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
+        NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
         NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
         NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
         NetworkFactory.addDenseLayer(network, LayerFactory.createDenseLayer(Tensor.TYPE.FLOAT, 10, 10));
@@ -62,38 +68,28 @@ public class LineRecognitionTest {
         NetworkLogic.randomizeWeights(network);
         System.out.println("LR, Activation, weights set...");
 
-        List<Sample> samples = new ArrayList<>();
-        for (int i = 0; i < TRAINING_SAMPLE_COUNT; i++) {
-            samples.add(SampleFactory.createSample(generateMatrixWithLines(0), new float[]{1f, 0f, 0f, 0f, 0f}));
-            samples.add(SampleFactory.createSample(generateMatrixWithLines(1), new float[]{0f, 1f, 0f, 0f, 0f}));
-            samples.add(SampleFactory.createSample(generateMatrixWithLines(2), new float[]{0f, 0f, 1f, 0f, 0f}));
-            samples.add(SampleFactory.createSample(generateMatrixWithLines(3), new float[]{0f, 0f, 0f, 1f, 0f}));
-            samples.add(SampleFactory.createSample(generateMatrixWithLines(4), new float[]{0f, 0f, 0f, 0f, 1f}));
-        }
-
-        List<Sample> controlsamples = new ArrayList<>();
-        for (int i = 0; i < TRAINING_SAMPLE_COUNT; i++) {
-            controlsamples.add(SampleFactory.createSample(generateMatrixWithLines(0), new float[]{1f, 0f, 0f, 0f, 0f}));
-            controlsamples.add(SampleFactory.createSample(generateMatrixWithLines(1), new float[]{0f, 1f, 0f, 0f, 0f}));
-            controlsamples.add(SampleFactory.createSample(generateMatrixWithLines(2), new float[]{0f, 0f, 1f, 0f, 0f}));
-            controlsamples.add(SampleFactory.createSample(generateMatrixWithLines(3), new float[]{0f, 0f, 0f, 1f, 0f}));
-            controlsamples.add(SampleFactory.createSample(generateMatrixWithLines(4), new float[]{0f, 0f, 0f, 0f, 1f}));
-        }
+        //samples
+        List<Sample> trainingSamples = new ArrayList<>();
+        generateSamples(trainingSamples, TRAINING_SAMPLE_COUNT);
+        
+        List<Sample> controlSamples = new ArrayList<>();
+        generateSamples(controlSamples, TRAINING_SAMPLE_COUNT);
         System.out.println("Samples added...");
 
+        //training
         System.out.println("fitting...");
-        NetworkLogic.fit(network, samples, 6000);
-
-        System.out.println("\nAfter training:\n");
+        NetworkLogic.fit(network, trainingSamples, 8000);
+        
         Layer outputLayer = NetworkUtils.getOutputLayer(network);
         double errorZero = 0;
         double errorOne = 0;
         double errorTwo = 0;
         double errorThree = 0;
         double errorFour = 0;
-
         int n = 0;
-        for (Sample sample : samples) {
+        
+        System.out.println("\nAfter training:\n");
+        for (Sample sample : trainingSamples) {
             NetworkLogic.propagate(network, sample.input);
             System.out.println("Oirinal: " + sample.target.getFloatValue(0) + ", " + sample.target.getFloatValue(1)
                     + ", " + sample.target.getFloatValue(2)
@@ -132,7 +128,7 @@ public class LineRecognitionTest {
         System.out.println("3: " + errorThree);
         System.out.println("4: " + errorFour);
 
-        for (Sample sample : controlsamples) {
+        for (Sample sample : controlSamples) {
             NetworkLogic.propagate(network, sample.input);
             System.out.println("Control: " + sample.target.getFloatValue(0) + ", " + sample.target.getFloatValue(1)
                     + ", " + sample.target.getFloatValue(2)
@@ -173,39 +169,67 @@ public class LineRecognitionTest {
         System.out.println("4: " + errorFour);
 
         //Live teszt
+        guessNumber(network, 10);
+
+        for (int i = 0; i < 5;i++) {
+            System.out.println(generateTensorWithLines(i).toString(true));
+            System.out.println();
+        }
+    }
+    
+    private static void generateSamples(Collection<Sample> samples, int count){
+        for (int i = 0; i < count; i++) {
+            samples.add(createLineSample());
+        }
+    }
+    
+    private static Sample createLineSample(){ //nem biztos hogy minden opcioval találkozik
+        int r = RANDOM_INT.nextInt(LINE_COUNT + 1); 
+        Tensor input = generateTensorWithLines(r);
+        Tensor target = Tensor.create(Tensor.TYPE.FLOAT, 5);
+        target.setFloatValue(1, r);
+        
+        return SampleFactory.createSample(input, target);
+    }
+    
+    public static void guessNumber(Network network, int numberOfMatrices) {
+        System.out.println("guessNumber teszt: ");
+
         Random rand = new Random();
-        for (int j = 0; j < 10; j++) {
-            System.out.println("Vizualis teszt: ");
+        int generatedNumber;
+        int guessedRight = 0;
+
+        for (int j = 0; j < numberOfMatrices; j++) {
+
             Layer testoutputLayer = NetworkUtils.getOutputLayer(network);
-            float[] testMatrix = generateMatrixWithLines(rand.nextInt(5));  //max 4
-            writeMatrixToConsole(testMatrix, 5, 5);
-            NetworkLogic.propagate(network, convertToTensor(testMatrix));
-            for (int i = 0; i < 5; i++) {
-                System.out.println(i + ":");
-                System.out.println(testoutputLayer.states.getFloatValue(i) * 100 + " %");
+            generatedNumber = rand.nextInt(5);  //max 4
+            Tensor testMatrix = generateTensorWithLines(generatedNumber);
+            System.out.println(testMatrix.toString(true));
+            NetworkLogic.propagate(network, testMatrix);
+
+            float max = testoutputLayer.states.getFloatValue(0);
+            int maxi = 0;
+            for (int i = 0; i < 4; i++) {
+                if (testoutputLayer.states.getFloatValue(i + 1) > max) {
+                    max = testoutputLayer.states.getFloatValue(i + 1);
+                    maxi = i + 1;
+                }
+            }
+
+            System.out.println("I see " + maxi + " line(s). (" + FORMAT.format(testoutputLayer.states.getFloatValue(maxi) * 100) + " %)");
+            System.out.println("//////////////");
+            if (generatedNumber == maxi) {
+                guessedRight++;
             }
         }
-
-        guessNumber(network, 10);
-//        //Matrix test
-//        for (int i = 0; i < 10; i++) {
-//            float[] tesztmatrix = generateMatrixWithLines(2);
-//            writeMatrixToConsole(tesztmatrix, 5, 5);
-//            System.out.println("");
-//        }
+        System.out.println("");
+        System.out.println("I guessed " + guessedRight + "/" + numberOfMatrices + " right.");
     }
-
-    public static float[] generateMatrixWithLines(int linecount) {
+    
+    public static Tensor generateTensorWithLines(int linecount) {
         int linelenght = LINE_LENGHT;
 
         float[][] matrix = new float[5][5];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-
-        int[] linedrows = new int[linecount];
         List<Integer> rows = new ArrayList<Integer>();
 
         Random rand = new Random();
@@ -233,93 +257,14 @@ public class LineRecognitionTest {
                         = 1f;
             }
         }
-
-        //flatten dimensions
-        ArrayList<Float> list = new ArrayList<Float>();
-
+        //convert to tensor
+        int counter = 0;
+        Tensor toReturn = Tensor.create(Tensor.TYPE.FLOAT, 5,5);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                list.add(matrix[i][j]);
+                toReturn.setFloatValue(matrix[j][i], counter++);
             }
-        }
-
-        float[] toReturn = new float[list.size()];
-        int i = 0;
-
-        for (Float f : list) {
-            toReturn[i++] = (f != null ? f : Float.NaN);
         }
         return toReturn;
-    }
-
-    public static void writeMatrixToConsole(float[][] matrix, int x, int y) {
-        for (int i = 0; i < x; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j < y; j++) {
-                System.out.print((int) matrix[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println("]");
-        }
-    }
-
-    public static void writeMatrixToConsole(float[] matrix, int x, int y) {
-        int counter = 0;
-        for (int i = 0; i < x; i++) {
-            System.out.print("[ ");
-            for (int j = 0; j < y; j++) {
-                System.out.print((int) matrix[counter++]);
-                System.out.print(" ");
-            }
-            System.out.println("]");
-        }
-    }
-
-    public static Tensor convertToTensor(float[] matrix) {
-        Tensor Test = Tensor.create(Tensor.TYPE.FLOAT, 25);
-        Test.setFloatArray(matrix);
-        return Test;
-    }
-
-    public static void guessNumber(Network network, int numberOfMatrices) {
-        System.out.println("guessNumber teszt: ");
-
-        Random rand = new Random();
-        int generatedNumber;
-        int guessedRight = 0;
-
-        for (int j = 0; j < numberOfMatrices; j++) {
-
-            Layer testoutputLayer = NetworkUtils.getOutputLayer(network);
-            generatedNumber = rand.nextInt(5);  //max 4
-            float[] testMatrix = generateMatrixWithLines(generatedNumber);
-            writeMatrixToConsole(testMatrix, 5, 5);
-            NetworkLogic.propagate(network, convertToTensor(testMatrix));
-
-            float max = testoutputLayer.states.getFloatValue(0);
-            int maxi = 0;
-            for (int i = 0; i < 4; i++) {
-                if (testoutputLayer.states.getFloatValue(i + 1) > max) {
-                    max = testoutputLayer.states.getFloatValue(i + 1);
-                    maxi = i + 1;
-                }
-            }
-//            System.out.println("");
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(testoutputLayer.states.getFloatValue(i));
-//            }
-
-            System.out.println("I see " + maxi + " line(s). (" + FORMAT.format(testoutputLayer.states.getFloatValue(maxi) * 100) + " %)");
-//            for (int i = 0; i < 5; i++) {
-//                System.out.println(i + ":");
-//                System.out.println(testoutputLayer.states.getFloatValue(i) * 100 + " %");
-//            }
-
-            if (generatedNumber == maxi) {
-                guessedRight++;
-            }
-
-        }
-        System.out.println("I guessed " + guessedRight + "/" + numberOfMatrices + " right.");
     }
 }
