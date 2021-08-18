480af7ebef28561cb48d9037b72921cc1e778d35
pestiantal <pesti.antal1@gmail.com>
2021-08-18 23:49:54 +0200

    Update SimpleConvTest.java

--- src/volgyerdo.test/classes/javacnn/tests/SimpleConvTest.java
+++ src/volgyerdo.test/classes/javacnn/tests/SimpleConvTest.java
@@ -24,31 +24,50 @@ import javacnn.data.DataBlock;
 import javacnn.layers.InputLayer;
 import javacnn.layers.Layer;
 import javacnn.layers.ConvolutionLayer;
+import javacnn.layers.MaxoutLayer;
+import javacnn.trainers.AdaDeltaTrainer;
+import javacnn.trainers.Trainer;
+
 /**
  *
  * @author User
  */
 public class SimpleConvTest {
+
     public static void main(String[] args) {
         List<Layer> layers = new ArrayList<>();
         OutputDefinition def = new OutputDefinition();
         def.setOutX(4);
         def.setOutY(4);
-        
-        layers.add(new InputLayer(def,4,4,1));
-        layers.add(new ConvolutionLayer(def,3,1,1,2)); //sx, filters, stride, padding
-        layers.add(new ConvolutionLayer(def,3,1,1,2)); //sx, filters, stride, padding
-        layers.add(new ConvolutionLayer(def,3,1,1,2)); //sx, filters, stride, padding
-        
+
+        layers.add(new InputLayer(def, 4, 4, 1));
+        layers.add(new ConvolutionLayer(def, 3, 1, 1, 2)); //sx, filters, stride, padding
+        layers.add(new ConvolutionLayer(def, 3, 1, 1, 2)); //sx, filters, stride, padding
+        layers.add(new MaxoutLayer(def)); //sx, filters, stride, padding
+
         JavaCNN net = new JavaCNN(layers);
-        
-        DataBlock db = new DataBlock(4,4,1,0);
-        net.forward(db, true);
-        int n = net.getBackPropagationResult().size();
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(net.getBackPropagationResult().get(i).getWeights()));
-        }
+
+        DataBlock db = new DataBlock(4, 4, 1, 0.0f); //int sx, int sy, int depth, double c
 
         
+        //net.backward(1);
+        db.setGradient(1, 1);
+        db.setWeight(1, 1);
+        int n = net.getBackPropagationResult().size();
+
+//        System.out.println(Arrays.toString(net.getBackPropagationResult().get(0).getWeights()));
+//        System.out.println("/////");
+        for (int i = 0; i < n; i++) {
+            System.out.println("-");
+            System.out.println(layers.get(i));
+
+        }
+        net.forward(db, true);
+        net.backward(0);
+        System.out.println(Arrays.toString(db.getGradients()));
+        System.out.println(Arrays.toString(db.getWeights()));
+        System.out.println("--");
+        
+
     }
 }
